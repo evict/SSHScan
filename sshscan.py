811f68eb013020f55b439995dfb532259cd4ad70
evict <vincentrr91@gmail.com>
2016-04-12 14:16:42 +0200

    Update sshscan.py

--- sshscan.py
+++ sshscan.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # The MIT License (MIT)
 # 
-# Copyright (c) 2015 Vincent Ruijter
+# Copyright (c) 2016 Vincent Ruijter
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
