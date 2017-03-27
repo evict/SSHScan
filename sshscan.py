bfa10f08c6d7d4b4ea0af9429a153beb7380ca81
evict <github@evicted.ninja>
2017-03-27 18:31:03 +0200

    removed RIPEMD fixed ASCII art

--- sshscan.py
+++ sshscan.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
 # The MIT License (MIT)
-# 
+#
 # Copyright (c) 2016 Vincent Ruijter
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -22,23 +22,23 @@
 # SOFTWARE.
 #
 # Cipher detection based on: https://stribika.github.io/2015/01/04/secure-secure-shell.html
-# 
+#
 
 import sys, re
 import socket
 from optparse import OptionParser, OptionGroup
 
 def banner():
-	banner = """
- _____ _____ _    _ _____                      
-/  ___/  ___| | | /  ___|                     
-\ `--.\ `--.| |_| \ `--.  ___ __ _ _ __  
-`--. \`--. |  _  |`--. \/ __/ _` | '_ \ 
-/\__/ /\__/ | | | /\__/ | (_| (_| | | | |
-\____/\____/\_| |_\____/ \___\__,_|_| |_|
-					evict
-			"""                                         
-	return banner
+        banner = """
+      _____ _____ _    _ _____
+     /  ___/  ___| | | /  ___|
+     \ `--.\ `--.| |_| \ `--.  ___ __ _ _ __
+      `--. \`--. |  _  |`--. \/ __/ _` | '_ \\
+     /\__/ /\__/ | | | /\__/ | (_| (_| | | | |
+     \____/\____/\_| |_\____/ \___\__,_|_| |_|
+                                            evict
+                """
+        return banner
 
 def exchange(ip, port):
 	try:
@@ -50,26 +50,26 @@ def exchange(ip, port):
 		print "    [+] Retrieving ciphers..."
 		ciphers = conn.recv(984)
 		conn.close()
-		
+
 		return ciphers
-	
+
 	except socket.timeout:
 		print "    [-] Timeout while connecting to %s on port %i\n"%(ip, port)
 		return False
-	
+
 	except socket.error as e:
 		if e.errno == 61:
 			print "    [-] %s\n"%(e.strerror)
 			pass
 		else:
 			print "    [-] Error while connecting to %s on port %i\n"%(ip, port)
-			return False	
+			return False
 
 def validate_target(target):
 	list = target.split(":")
 	if len(list) != 1 and len(list) != 2: # only valid states
 		print "[-] %s it not a valid target!"%target
-		return False	
+		return False
 	hostname = list[0]
 	if len(hostname) > 255:
 		print "[-] %s it not a valid target!"%target
@@ -98,21 +98,21 @@ def parse_target(target):
 		if not re.search(r'[:*]', target):
 			print "[*] Target %s specified without a port number, using default port 22"%target
 			target = target+':22'
-	
+
 		ipport=target.split(':')
 
 		try:
 			print "[*] Initiating scan for %s on port %s" %(ipport[0], ipport[1])
 			if not get_output(exchange(ipport[0], int(ipport[1]))):
 				return False
-	
+
 		except IndexError:
 			print "    [-] Please specify target as 'target:port'!\n"
-			return False	
-	
+			return False
+
 		except ValueError:
 			print "    [-] Target port error, please specify a valid port!\n"
-			return False	
+			return False
 
 def list_parser(list):
 	try:
@@ -147,9 +147,9 @@ def get_output(rawlist):
 		ciphers = ['3des-cbc','aes128-cbc','aes192-cbc','aes256-cbc','aes128-ctr','aes192-ctr','aes256-ctr','aes128-gcm@openssh.com','aes256-gcm@openssh.com','arcfour','arcfour128','arcfour256','blowfish-cbc','cast128-cbc','chacha20-poly1305@openssh.com']
 		strong_ciphers = ['chacha20-poly1305@openssh.com','aes256-gcm@openssh.com','aes128-gcm@openssh.com','aes256-ctr','aes192-ctr','aes128-ctr']
 		weak_ciphers = []
-	   	macs = ['hmac-md5','hmac-md5-96','hmac-ripemd160','hmac-sha1','hmac-sha1-96','hmac-sha2-256','hmac-sha2-512','umac-64','hmac-md5-etm@openssh.com','hmac-md5-96-etm@openssh.com','hmac-ripemd160-etm@openssh.com','hmac-sha1-etm@openssh.com','hmac-sha1-96-etm@openssh.com','hmac-sha2-256-etm@openssh.com','hmac-sha2-512-etm@openssh.com','umac-64-etm@openssh.com','umac-128-etm@openssh.com']	
-		strong_macs = ['hmac-sha2-512-etm@openssh.com','hmac-sha2-256-etm@openssh.com','hmac-ripemd160-etm@openssh.com','umac-128','umac-128-etm@openssh.com','hmac-sha2-512','hmac-sha2-256','hmac-ripemd160','umac-128@openssh.com']
-		weak_macs = [] 
+	   	macs = ['hmac-md5','hmac-md5-96','hmac-ripemd160','hmac-sha1','hmac-sha1-96','hmac-sha2-256','hmac-sha2-512','umac-64','hmac-md5-etm@openssh.com','hmac-md5-96-etm@openssh.com','hmac-ripemd160-etm@openssh.com','hmac-sha1-etm@openssh.com','hmac-sha1-96-etm@openssh.com','hmac-sha2-256-etm@openssh.com','hmac-sha2-512-etm@openssh.com','umac-64-etm@openssh.com','umac-128-etm@openssh.com']
+		strong_macs = ['hmac-sha2-512-etm@openssh.com','hmac-sha2-256-etm@openssh.com','umac-128','umac-128-etm@openssh.com','hmac-sha2-512','hmac-sha2-256','umac-128@openssh.com']
+		weak_macs = []
 		kex = ['curve25519-sha256@libssh.org','diffie-hellman-group1-sha1','diffie-hellman-group14-sha1','diffie-hellman-group-exchange-sha1','diffie-hellman-group-exchange-sha256','ecdh-sha2-nistp256','ecdh-sha2-nistp384','ecdh-sha2-nistp521','ecdsa-sha2-nistp256-cert-v01@openssh.com','ecdsa-sha2-nistp384-cert-v01@openssh.com','ecdsa-sha2-nistp521-cert-v01@openssh.com']
 		strong_kex = ['curve25519-sha256@libssh.org', 'diffie-hellman-group-exchange-sha256']
 		weak_kex = []
@@ -187,46 +187,46 @@ def get_output(rawlist):
 		compression = False
 		if re.search("zlib@openssh.com", rawlist):
 			compression = True
-		print '    [+] Detected the following ciphers: '  			
+		print '    [+] Detected the following ciphers: '
 		print_columns(dciphers)
-		print '    [+] Detected the following KEX algorithms: '  			
+		print '    [+] Detected the following KEX algorithms: '
 		print_columns(dkex)
-		print '    [+] Detected the following MACs: '  			
+		print '    [+] Detected the following MACs: '
 		print_columns(dmacs)
-		print '    [+] Detected the following HostKey algorithms: '  			
+		print '    [+] Detected the following HostKey algorithms: '
 		print_columns(dhka)
 
 		if weak_ciphers:
 			print '    [+] Detected the following weak ciphers: '
 			print_columns(weak_ciphers)
 		else:
-			print '    [+] No weak ciphers detected!'	
+			print '    [+] No weak ciphers detected!'
 
 		if weak_kex:
 			print '    [+] Detected the following weak KEX algorithms: '
 			print_columns(weak_kex)
 		else:
-			print '    [+] No weak KEX detected!'	
+			print '    [+] No weak KEX detected!'
 
 		if weak_macs:
 			print '    [+] Detected the following weak MACs: '
 			print_columns(weak_macs)
 		else:
-			print '    [+] No weak MACs detected!'	
+			print '    [+] No weak MACs detected!'
 
 		if weak_hka:
 			print '    [+] Detected the following weak HostKey algorithms: '
 			print_columns(weak_hka)
 		else:
-			print '    [+] No weak HostKey algorithms detected!'	
+			print '    [+] No weak HostKey algorithms detected!'
 
 		if compression == True:
 			print "    [+] Compression has been enabled!"
-		
+
 		return True
 
 def print_columns(cipherlist):
-	# adjust the amount of columns to display 
+	# adjust the amount of columns to display
 	cols = 2
 	while len(cipherlist) % cols != 0:
 		cipherlist.append('')
@@ -235,13 +235,13 @@ def print_columns(cipherlist):
 		for row in zip(*split):
 			print "            " + "".join(str.ljust(c,37) for c in row)
 	print "\n"
-	
+
 def main():
 	try:
 		print banner()
 		parser = OptionParser(usage="usage %prog [options]", version="%prog 1.0")
 		parameters = OptionGroup(parser, "Options")
-	
+
 		parameters.add_option("-t", "--target", type="string", help="Specify target as 'target' or 'target:port' (port 22 is default)", dest="target")
 		parameters.add_option("-l", "--target-list", type="string", help="File with targets: 'target' or 'target:port' seperated by a newline (port 22 is default)", dest="targetlist")
 		parser.add_option_group(parameters)
@@ -252,7 +252,7 @@ def main():
 		targetlist = options.targetlist
 
 		if target:
-			parse_target(target)		
+			parse_target(target)
 		else:
 			if targetlist:
 				list_parser(targetlist)
@@ -262,7 +262,7 @@ def main():
 
 	except KeyboardInterrupt:
 		print "\n[-] ^C Pressed, quitting!"
-		sys.exit(3)		
+		sys.exit(3)
 
 if __name__ == '__main__':
 	main()
